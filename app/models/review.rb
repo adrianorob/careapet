540006af62f112455c7767b75c2f1994bdb5cde4
cesarfuster <cfuster@uol.com.br>
2018-04-27 11:07:28 -0300

    Review validates

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,7 @@ class Review < ApplicationRecord
 
   validates :rating, :inclusion => { :in => 0..5 }
   validates :rating, numericality: { only_integer: true }
+  validates :rating, presence: true
+  validates :description, presence: true
 
 end
