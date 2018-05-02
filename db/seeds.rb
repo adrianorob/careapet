a59d7b4f9d43d47a0ad0073d70e6fd663d008c24
cesarfuster <cfuster@uol.com.br>
2018-05-02 10:12:30 -0300

    Seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ Task.create(name: "Adestrar")
 Task.create(name: "Pernoite")
 
 puts "2 Usuários caregiver: true"
-u1 = User.create(first_name: "caregiver1", last_name: "caregiver1", address: "Rua Mourato Coelho, 1404 - Pinheiros, São Paulo", photo: "image/upload/v1525085742/dzkieig7dey02ayplykp.png", email: "caregiver1@caregiver1.com", phone_number: "9999-9999", caregiver: true)
-u2 = User.create(first_name: "caregiver2", last_name: "caregiver2", address: "Rua Domingos de Morais, 2450 - Vila Mariana", photo: "image/upload/v1525085814/ryhltgbcbgxyv0xvsi5d.png", email: "caregiver2@caregiver2.com", phone_number: "", caregiver: true)
+u1 = User.create(first_name: "caregiver1", last_name: "caregiver1", address: "Rua Mourato Coelho, 1404 - Pinheiros, São Paulo", photo: "image/upload/v1525085742/dzkieig7dey02ayplykp.png", email: "caregiver1@caregiver1.com", phone_number: "9999-9999", caregiver: true, password: '123456', password_confirmation: '123456')
+u2 = User.create(first_name: "caregiver2", last_name: "caregiver2", address: "Rua Domingos de Morais, 2450 - Vila Mariana", photo: "image/upload/v1525085814/ryhltgbcbgxyv0xvsi5d.png", email: "caregiver2@caregiver2.com", phone_number: "", caregiver: true, password: '123456', password_confirmation: '123456')
 
 puts "3 user_tasks caregiver 1"
 ust1 = UserTasks.create(task_id: 5, user_id: 1, price: "1000")
@@ -27,9 +27,9 @@ puts "1 user_tasks caregiver 2"
 ust4 = UserTasks.create(task_id: 5, user_id: 2, price: "90")
 
 puts "3 Usuários caregiver: false, todos compradores"
-u3 = User.create(first_name: "teste", last_name: "teste", address: "Rua Coronel Oscar Porto, 70 - Paraíso, São Paulo", photo: "image/upload/v1525085951/ovrl0xbgibzkxeegfysi.png", email: "teste@teste.com", phone_number: "9999-9999", caregiver: false)
-u4 = User.create(first_name: "César", last_name: "Fuster", address: "Rua Nanuque, 115 - Vila Leopoldina, São Paulo", photo: "image/upload/v1525086012/amm2lf1z3fxxsbt1ucm6.jpg", email: "cesar@cesar.com", phone_number: "9999-9999", caregiver: false)
-u5 = User.create(first_name: "teste2", last_name: "teste2", address: "Avenida Paulista, 900 - Bela Vista, São Paulo", photo: "image/upload/v1525262960/qqizyirxigpjmsoen62a.png", email: "teste2@teste2.com", phone_number: "9999-9999", caregiver: false)
+u3 = User.create(first_name: "teste", last_name: "teste", address: "Rua Coronel Oscar Porto, 70 - Paraíso, São Paulo", photo: "image/upload/v1525085951/ovrl0xbgibzkxeegfysi.png", email: "teste@teste.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
+u4 = User.create(first_name: "César", last_name: "Fuster", address: "Rua Nanuque, 115 - Vila Leopoldina, São Paulo", photo: "image/upload/v1525086012/amm2lf1z3fxxsbt1ucm6.jpg", email: "cesar@cesar.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
+u5 = User.create(first_name: "teste2", last_name: "teste2", address: "Avenida Paulista, 900 - Bela Vista, São Paulo", photo: "image/upload/v1525262960/qqizyirxigpjmsoen62a.png", email: "teste2@teste2.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
 
 
 
