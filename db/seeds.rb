d0935a5d79dfedea77e7aa7f2ba4a9214d8749da
Adriano Barroso <wiermann@broou.com>
2018-05-02 16:03:37 -0300

    fix merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,13 +27,7 @@ puts "1 user_tasks caregiver 2"
 ust4 = UserTask.create(task_id: 5, user_id: 2, price: "90")
 
 puts "3 Usuários caregiver: false, todos compradores"
-u3 = User.create(first_name: "teste", last_name: "teste", address: "Rua Coronel Oscar Porto, 70 - Paraíso, São Paulo", email: "teste@teste.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
-u4 = User.create(first_name: "César", last_name: "Fuster", address: "Rua Nanuque, 115 - Vila Leopoldina, São Paulo", email: "cesar@cesar.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
-u5 = User.create(first_name: "teste2", last_name: "teste2", address: "Avenida Paulista, 900 - Bela Vista, São Paulo", email: "teste2@teste2.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
 
-
-
-
-
-
-
+u3 = User.create(first_name: "teste", last_name: "teste", address: "Rua Coronel Oscar Porto, 70 - Paraíso, São Paulo", photo: "image/upload/v1525085951/ovrl0xbgibzkxeegfysi.png", email: "teste@teste.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
+u4 = User.create(first_name: "César", last_name: "Fuster", address: "Rua Nanuque, 115 - Vila Leopoldina, São Paulo", photo: "image/upload/v1525086012/amm2lf1z3fxxsbt1ucm6.jpg", email: "cesar@cesar.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
+u5 = User.create(first_name: "teste2", last_name: "teste2", address: "Avenida Paulista, 900 - Bela Vista, São Paulo", photo: "image/upload/v1525262960/qqizyirxigpjmsoen62a.png", email: "teste2@teste2.com", phone_number: "9999-9999", caregiver: false, password: '123456', password_confirmation: '123456')
