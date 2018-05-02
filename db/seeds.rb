900de2d348a809169d5c77066bf2dbaf504fa8b1
cesarfuster <cfuster@uol.com.br>
2018-05-02 10:09:19 -0300

    Seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+puts "Tasks..."
 Task.create(name: "Companhia")
 Task.create(name: "Passear")
 Task.create(name: "Levar ao PetShop")
 Task.create(name: "Levar ao Veterinário")
 Task.create(name: "Adestrar")
 Task.create(name: "Pernoite")
+
+puts "2 Usuários caregiver: true"
+u1 = User.create(first_name: "caregiver1", last_name: "caregiver1", address: "Rua Mourato Coelho, 1404 - Pinheiros, São Paulo", photo: "image/upload/v1525085742/dzkieig7dey02ayplykp.png", email: "caregiver1@caregiver1.com", phone_number: "9999-9999", caregiver: true)
+u2 = User.create(first_name: "caregiver2", last_name: "caregiver2", address: "Rua Domingos de Morais, 2450 - Vila Mariana", photo: "image/upload/v1525085814/ryhltgbcbgxyv0xvsi5d.png", email: "caregiver2@caregiver2.com", phone_number: "", caregiver: true)
+
+puts "3 user_tasks caregiver 1"
+ust1 = UserTasks.create(task_id: 5, user_id: 1, price: "1000")
+ust2 = UserTasks.create(task_id: 6, user_id: 1, price: "50")
+ust3 = UserTasks.create(task_id: 2, user_id: 1, price: "100")
+
+puts "1 user_tasks caregiver 2"
+ust4 = UserTasks.create(task_id: 5, user_id: 2, price: "90")
+
+puts "3 Usuários caregiver: false, todos compradores"
+u3 = User.create(first_name: "teste", last_name: "teste", address: "Rua Coronel Oscar Porto, 70 - Paraíso, São Paulo", photo: "image/upload/v1525085951/ovrl0xbgibzkxeegfysi.png", email: "teste@teste.com", phone_number: "9999-9999", caregiver: false)
+u4 = User.create(first_name: "César", last_name: "Fuster", address: "Rua Nanuque, 115 - Vila Leopoldina, São Paulo", photo: "image/upload/v1525086012/amm2lf1z3fxxsbt1ucm6.jpg", email: "cesar@cesar.com", phone_number: "9999-9999", caregiver: false)
+u5 = User.create(first_name: "teste2", last_name: "teste2", address: "Avenida Paulista, 900 - Bela Vista, São Paulo", photo: "image/upload/v1525262960/qqizyirxigpjmsoen62a.png", email: "teste2@teste2.com", phone_number: "9999-9999", caregiver: false)
+
+
+
+
+
+
+
